3ab9b1f5ee7b31e42150b357d36c116222754733
sdahal1 <raabdahl@gmail.com>
2024-01-29 15:56:48 -0800

    create new post feature works

--- src/_root/pages/index.ts
+++ src/_root/pages/index.ts
@@ -1,1 +1,19 @@
 export {default as Home} from './Home';
+export {default as Explore} from './Explore';
+// saved
+export {default as Saved} from './Saved';
+// createpost
+export {default as CreatePost} from './CreatePost';
+//profile
+export {default as Profile} from './Profile';
+//updateProfile
+export {default as UpdateProfile} from './UpdateProfile';
+//editPost
+export {default as EditPost} from './EditPost';
+//postDetails
+export {default as PostDetails} from './PostDetails';
+//allusers
+export {default as AllUsers} from './AllUsers';
+//likedPosts
+export {default as LikedPosts} from './LikedPosts';
+
--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -4,8 +4,9 @@ import {
   useQueryClient, // used for invalidating the cache
   useInfiniteQuery, // used for fetching paginated data
 } from '@tanstack/react-query';
-import { createUserAccount, signInAccount, signOutAccount } from '../appwrite/api';
-import { INewUser } from '@/types';
+import { createPost, createUserAccount, signInAccount, signOutAccount } from '../appwrite/api';
+import { INewPost, INewUser } from '@/types';
+import { QUERY_KEYS } from './queryKeys';
 
 
 export const useCreateUserAccount = () => {
@@ -25,5 +26,18 @@ export const useSignInAccount = () => {
 export const useSignOutAccount = () => {
   return useMutation({
     mutationFn: signOutAccount
+  })
+}
+
+export const useCreatePost = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (post: INewPost) => createPost(post),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+      });
+    }
   })
 }
